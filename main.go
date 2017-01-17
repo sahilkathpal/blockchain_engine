4160d6d866936784216ba80802dea7d103c1e3ae
sahil <sahil@esscale.co>
2017-01-17 13:24:52 +0530

    created main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+
+	. "github.com/tendermint/go-common"
+	middleware "bitbucket.org/sahilkathpal06/blockchain_engine"
+	"github.com/tendermint/tmsp/server"
+	"github.com/tendermint/tmsp/types"
+)
+
+func main() {
+
+	addrPtr := flag.String("addr", "tcp://0.0.0.0:46658", "Listen address")
+	tmspPtr := flag.String("tmsp", "socket", "socket | grpc")
+	persistencePtr := flag.String("persist", "", "directory to use for a database")
+	flag.Parse()
+
+	// Create the application - in memory or persisted to disk
+	var app types.Application
+	app = middleware.NewMiddlewareApplication()
+
+	// Start the listener
+	srv, err := server.NewServer(*addrPtr, *tmspPtr, app)
+	if err != nil {
+		Exit(err.Error())
+	}
+
+	// Wait forever
+	TrapSignal(func() {
+		// Cleanup
+		srv.Stop()
+	})
+
+}
